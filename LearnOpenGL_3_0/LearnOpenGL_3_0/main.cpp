37bd641a82a4ea872ec489ed03d3a332c99f368c
khanhdrag9 <tkhanh783@gmail.com>
2018-08-07 16:30:14 +0700

    .

--- LearnOpenGL_3_0/LearnOpenGL_3_0/main.cpp
+++ LearnOpenGL_3_0/LearnOpenGL_3_0/main.cpp
@@ -1,2 +1,29 @@
+
 #include "glad\glad.h"
 #include "GLFW\glfw3.h"
+
+#include <stdio.h>
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Learn Open GL part3-lession1", NULL, NULL);
+	if (window == NULL)
+	{
+		printf("Fail to create Window\n");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		printf("Fail to init Glad\n");
+		return -1;
+	}
+
+	return 0;
+}
