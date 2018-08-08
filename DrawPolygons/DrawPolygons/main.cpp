779010af241f2aeda0811f556f1237ba2260ce13
khanhdrag9 <tkhanh783@gmail.com>
2018-08-08 20:45:13 +0700

    [UPDATE] main.cpp & glad.cpp

--- DrawPolygons/DrawPolygons/main.cpp
+++ DrawPolygons/DrawPolygons/main.cpp
@@ -1,3 +1,40 @@
 #include <stdio.h>
-#include "GLFW/glfw3.h"
 #include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Draw polygon", NULL, NULL);
+	if (window == NULL)
+	{
+		printf("Fail to create window!");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		printf("Fail to init Glad!");
+		return -1;
+	}
+	glClearColor(0.8, 0.4, 0.6, 1.0);
+
+
+	//loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		glfwSwapBuffers(window);
+	}
+
+
+	glfwTerminate();
+	return 0;
+}
